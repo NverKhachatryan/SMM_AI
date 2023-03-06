07e8499facb1f7ba0275946a2f118528cc0b1837
NverKhachatryan1 <nver@isolufy.com>
2023-03-07 00:58:58 +0400

    feat: added email auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,6 +6,7 @@ import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import EmailProvider from 'next-auth/providers/email';
 
 const prisma = new PrismaClient();
 
@@ -21,6 +22,17 @@ export default NextAuth({
           response_type: 'code',
         },
       },
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.SMTP_FROM,
     }),
   ],
   secret: process.env.SECRET,
